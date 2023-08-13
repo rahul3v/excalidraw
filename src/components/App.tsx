c21fef84f4e88759be257e2c8e8f20cafada7b10
Rahul <rahul3v@users.noreply.github.com>
2023-08-13 16:16:29 +0530

    Limiting the binding to fix performance issue
    Limiting the binding to fix performance issue

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7379,6 +7379,7 @@ class App extends React.Component<AppProps, AppState> {
   private maybeSuggestBindingForAll(
     selectedElements: NonDeleted<ExcalidrawElement>[],
   ): void {
+    if (selectedElements.length > 50) return;
     const suggestedBindings = getEligibleElementsForBinding(selectedElements);
     this.setState({ suggestedBindings });
   }
